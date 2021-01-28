4350262bff427eaddc6a35352fac9aded3120f50
my-name <my-email>
2021-01-28 17:28:57 +0530

    nnn

---
+++ Documents/JTD/hello.py
@@ -0,0 +1,11 @@
+n=int(input('enter any value'))
+i=0
+count=i+1
+for i in range(0,n):
+    while n%2==0:
+        print (n)
+       
+        for j in range(i):
+            while n%2!=0:
+                print(j)
+                   
